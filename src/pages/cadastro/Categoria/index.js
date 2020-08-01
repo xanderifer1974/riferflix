137117cf665e126b6efd4c1bbe4ad0718a539772
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2020-07-31 21:57:33 -0300

    Aula 3 - Formulario Cadastro de Categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,22 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from '../../../components/PageDefault'
 import { Link } from 'react-router-dom'
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: ''
+  }
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    })
+  }
+
+  //Função genérica de manipulação
+  function handleChange(evento) {
+    setValue(
+      evento.target.getAttribute('name'),
+      evento.target.value);
+  }
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type="text" />
-        </label>
+      <form onSubmit={function handleSubmit(infoEvento) {
+        infoEvento.preventDefault(); //Previne o envio de formulário padrão.
+        setCategorias([ //Adiciona uma categoria nova na lista.
+          ...categorias,
+          values
+        ]);
+
+        setValue(valoresIniciais);
+      }}>
+
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+
+          label="Descrição:"
+          type="????"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
         <button>
           Cadastrar
         </button>
       </form>
-      
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          )
+        })}
+      </ul>
+
       <Link to="/">
         Ir para Home
           </Link>
